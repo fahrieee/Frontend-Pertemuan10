7d60507821204c3af105ddc75b185509e6ea4723
Fahri <59986899+fahrieee@users.noreply.github.com>
2023-06-01 23:35:12 +0700

    Pertemuan10

---
+++ src/components/ui/Button/index.js
@@ -0,0 +1,16 @@
+import styled, { css } from "styled-components";
+
+const Button = styled.button`
+  padding: 0.8rem 2rem;
+  border: none;
+  border-radius: 10px;
+  color: #fff;
+  cursor: pointer;
+
+  // PROPS VARIANT
+  background-color: ${(props) =>
+    props.theme.colors[props.variant] || props.theme.colors["primary"]};
+
+`;
+
+export default Button;
